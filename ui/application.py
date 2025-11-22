654da60adc31c70585b171a7e0444dd60e37a088
Rosie Bahrani <rosie.bahrani@gmail.com>
2025-11-22 16:27:57 -0500

    update the submit button to route to results page

--- ui/application.py
+++ ui/application.py
@@ -1,4 +1,10 @@
+import base64
+import io
+
 from flask import Flask, request, render_template
+import yfinance
+import json
+import matplotlib.pyplot as plt
 import pickle
 from datetime import datetime
 
@@ -12,8 +18,29 @@ def render_home_page():
     list_of_tickers = ['AAPL', 'GOOG', 'TSLA', 'AMZN', 'META']
     selected_ticker = request.form.get('selected_ticker')
     if request.method == 'POST':
-        print(selected_ticker)
-        return render_template("results.html", selected_ticker=selected_ticker)
+        data = yfinance.Ticker(selected_ticker).history(period='1mo')
+        close_prices = data['Close']
+
+        # PLT code
+        plt.style.use('dark_background')
+        fig, ax = plt.subplots(figsize=(20, 10))
+        ax.plot(data.index, close_prices, linewidth=3)
+        ax.set_title(f'{selected_ticker} – Last 30 Days (Close Prices)', fontsize=12)
+        ax.set_xlabel('Date', fontsize=8)
+        ax.set_ylabel('Price $', fontsize=15)
+
+        # Save to in-memory buffer
+        buf = io.BytesIO()
+        fig.savefig(buf, format='png', bbox_inches='tight')
+        buf.seek(0)
+        plt.close(fig)
+
+        # Encode as base64 string
+        img_base64 = base64.b64encode(buf.getvalue()).decode('utf-8')
+        plot_url = f"data:image/png;base64,{img_base64}"
+
+        html_table = data.to_html(classes='stock-table', border=1, justify='left')
+        return render_template("results.html", selected_ticker=selected_ticker, html_table=html_table, plot_url=plot_url)
     return render_template("homepage.html", list_of_tickers=list_of_tickers)
 
 # @application.route('/resume', methods=['GET'])
