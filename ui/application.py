def49960ec54797876166a27618051710a3eacb6
Rosie Bahrani <rosie.bahrani@gmail.com>
2025-11-22 15:45:15 -0500

    update the submit button to route to results page

--- ui/application.py
+++ ui/application.py
@@ -10,10 +10,10 @@ def predict(ticker):
 @application.route('/', methods=['GET', 'POST'])
 def render_home_page():
     list_of_tickers = ['AAPL', 'GOOG', 'TSLA', 'AMZN', 'META']
-    selected_ticker = request.form.get('ticker')
+    selected_ticker = request.form.get('selected_ticker')
     if request.method == 'POST':
         print(selected_ticker)
-        return render_template("results.html")
+        return render_template("results.html", selected_ticker=selected_ticker)
     return render_template("homepage.html", list_of_tickers=list_of_tickers)
 
 # @application.route('/resume', methods=['GET'])
